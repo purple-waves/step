392947c831536b47d2871531b285f0e0a44e5469
Rachel Schroder <rachel.schroder@bigpond.com>
2020-12-10 23:13:56 +0000

    Change serialUID from default to generated

--- walkthroughs/week-3-server/examples/todo-list/src/main/java/com/google/sps/servlets/ListTasksServlet.java
+++ walkthroughs/week-3-server/examples/todo-list/src/main/java/com/google/sps/servlets/ListTasksServlet.java
@@ -34,7 +34,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/list-tasks")
 public class ListTasksServlet extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = -7562578299606814009L;
 
     @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
