4477383f83caed17d669c9ef4d003b010a2ddd93
Rachel Schroder <rachel.schroder@bigpond.com>
2020-12-11 03:21:16 +0000

    Remove obsolete getGreeting function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -36,14 +36,6 @@ function addRandomQuote() {
   quoteAuthor.innerText = "- " + author;
 }
 
-function getGreeting() {
-  console.log("Fetching the greeting");
-
-  fetch('/data').then(response => response.text()).then((greeting) => {
-    document.getElementById('greeting-container').innerText = greeting;
-  });
-}
-
 function getComments() {
     fetch('/data').then(response => response.json()).then((comments) => {
     commentsContainer = document.getElementById("comments-container");
