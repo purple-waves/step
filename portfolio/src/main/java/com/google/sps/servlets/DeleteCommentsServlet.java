75d57243663915d91a05423daf32d7da0de42778
Rachel Schroder <rachel.schroder@bigpond.com>
2020-12-31 03:21:54 +0000

    Disable functionality for deleting all comments data

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentsServlet.java
@@ -1,18 +1,18 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
-import java.util.Iterator;
+// import java.util.Iterator;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.Query;
+// import com.google.appengine.api.datastore.DatastoreService;
+// import com.google.appengine.api.datastore.DatastoreServiceFactory;
+// import com.google.appengine.api.datastore.Entity;
+// import com.google.appengine.api.datastore.Key;
+// import com.google.appengine.api.datastore.Query;
 
 /** Servlet that deletes all comments from datastore */
 @WebServlet("/delete-data")
@@ -22,16 +22,19 @@ public class DeleteCommentsServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Query query = new Query("Comment");
+    // this functionality has been disabled so users cannot delete all of the comments
 
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Iterator<Entity> results = datastore.prepare(query).asIterator();
+    // Query query = new Query("Comment");
 
-    while (results.hasNext()) {
-      Entity comment = results.next();
-      Key key = comment.getKey();
-      datastore.delete(key);
-    }
+    // DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    // Iterator<Entity> results = datastore.prepare(query).asIterator();
+
+
+    // while (results.hasNext()) {
+    //   Entity comment = results.next();
+    //   Key key = comment.getKey();
+    //   datastore.delete(key); 
+    // }
 
     response.sendRedirect("/comments.html");
   }
